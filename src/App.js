f5a71104d53883484fd51612afcbe6646b3c4de6
rahull0328 <rmmehta2004@gmail.com>
2024-11-30 01:47:24 +0530

    Backend Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import CurrentLocation from './currentLocation';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <React.Fragment>
+      <div className='container'>
+        <CurrentLocation />
+      </div>
+      Developed by{" "}
+        <a href="https://www.rahull0328.github.io">
+          Rahul Mehta
         </a>
-      </header>
-    </div>
+        <a href="https://www.heetmehta18.github.io">
+          Heet Mehta
+        </a>
+        <a href="https://www.github.com/urvalkheni">
+          Urval Kheni
+        </a>
+    </React.Fragment>
   );
 }
 
---
+++ src/apikey.js
@@ -0,0 +1,4 @@
+module.exports = {
+    key: "cac6c4a3d8eec692dce400b3823c6e68",
+    base: "https://api.openweathermap.org/data/2.5/",
+};
